5b9844d45fc296f75523f434793d0484b1c52a2c
loaizavelez <miguel.loaizav@upb.edu.co>
2025-07-24 09:43:26 -0500

    punto

--- Actividad1/Reto/Punto11.asm
+++ Actividad1/Reto/Punto11.asm
@@ -22,6 +22,6 @@ M=M-1
 1. La función de este codigo es hacer que la variable 100 vaya decrementando dentro del loop en 1 unidad, cuando llegue a 0 el loop se dentendra.
 2. La variable i esta guardada en la dirección @16 de la RAM:
 3. Los comentarios no se guardan dentro del ensamblador, unicamente sirven como guía. 
-4. La primera instrucción es el @1000
-5.
-6.
+4. La primera instrucción es el @1000, alamcenada en la direccion RAM 1000
+5.Loop es una etiqueta para continuar el ciclo si se cumple la condición, CONT es la finalización del loop debido a que ya no se cumple la condición
+6. i es la variable 1000 y CONT es la etiqueta.
