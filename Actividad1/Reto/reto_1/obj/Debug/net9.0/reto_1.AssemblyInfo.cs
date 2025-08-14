92bbe74c8eba8356c9859d965fb0a7514efbcdc5
loaizavelez <miguel.loaizav@upb.edu.co>
2025-08-14 09:32:02 -0500

    correcion actividad 2

--- Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
+++ Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6c54b06d1bf546609115f4ae9eeae3ad87aae6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc185b0a72d605e2dc5c48342f4229049dae4b9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unidad_3/Source_actividad2/src/ofApp.cpp
+++ Unidad_3/Source_actividad2/src/ofApp.cpp
@@ -2,9 +2,9 @@
 
 
 //--------------------------------------------------------------
-void ofApp::setup() { // Initialize the application
-	ofBackground(8); // // // Set the background color to a dark shade
-	particleColor = ofColor::white; // Initialize the particle color to white
+void ofApp::setup() { 
+	ofBackground(8); 
+	particleColor = ofColor::white; 
 }
 
 //--------------------------------------------------------------
@@ -14,21 +14,21 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() { // Draw particles
-	for (auto& pos : particles) { // Loop through each particle position
-		ofSetColor(particleColor); // Set the color for the particles
-		ofDrawCircle(pos.x, pos.y, 50); // Draw a circle at the particle position
+	for (auto& pos : particles) { 
+		ofSetColor(particleColor); 
+		ofDrawCircle(pos.x, pos.y, 50); 
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) { // Add a new particle at the mouse position
-	particles.push_back(ofVec2f(x, y)); // Store the mouse position as a new particle
-	if (particles.size() > 100) { // Limit the number of particles to 100
-		particles.erase(particles.begin()); // Remove the oldest particle if the limit is exceeded
+void ofApp::mouseMoved(int x, int y) { 
+	particles.push_back(ofVec2f(x, y)); 
+	if (particles.size() > 100) { 
+		particles.erase(particles.begin()); 
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) { // Change the particle color when the mouse is pressed
-	particleColor = ofColor(ofRandom(255), ofRandom(255 ), ofRandom(255)); // Generate a random color for the particles
+void ofApp::mousePressed(int x, int y, int button) { 
+	particleColor = ofColor(ofRandom(255), ofRandom(255 ), ofRandom(255)); 
 }
