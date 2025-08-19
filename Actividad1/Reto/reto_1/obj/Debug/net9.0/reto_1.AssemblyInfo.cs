9d8e42757bea8ce9a628b89c636181f9b201c34d
loaizavelez <miguel.loaizav@upb.edu.co>
2025-08-19 09:05:10 -0500

    esferas

--- Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
+++ Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc185b0a72d605e2dc5c48342f4229049dae4b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bbe74c8eba8356c9859d965fb0a7514efbcdc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unidad_3/Investigación.md
+++ Unidad_3/Investigación.md
@@ -22,3 +22,26 @@
 
 
 - ¿Qué hace la función draw?
+
+
+### Actividad 3
+
+### Actividad 4
+
+### Actividad 5
+
+
+### Actividad 6
+
+R// El error es que la esfera es seleccionada acompaña al puntero del mouse causando que no se puedan seleccionar ninguna otra esfera, en ocasiones permite coger un par de esferas , pero, no tiene una función para cuando se desselecciona, causando que la comparativa del radio de la esfera siempre este 
+
+### Actividad 7
+
+
+### Actividad 8
+
+### Actividad 9
+
+
+### Actividad 10
+
