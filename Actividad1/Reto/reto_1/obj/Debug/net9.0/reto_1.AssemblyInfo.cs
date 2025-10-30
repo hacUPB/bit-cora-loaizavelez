65e8f89c00f98883e3eaade59312ae6487ce764f
loaizavelez <miguel.loaizav@upb.edu.co>
2025-10-29 20:17:47 -0500

    retos

--- Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
+++ Actividad1/Reto/reto_1/obj/Debug/net9.0/reto_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2a9e89bd7e311fdfabd1c4f467368080b9e6e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848f18c8edd98cd7605c3972c186d7fd7625e59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reto_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
